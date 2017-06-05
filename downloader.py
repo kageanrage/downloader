8058ab15d17c0e6b897924b04c9fbc2286d98e46
kageanrage <kevinjpickett@gmail.com>
2017-06-06 08:44:16 +1000

    simplified check for video files and added code for file move

--- downloader.py
+++ downloader.py
@@ -1,16 +1,24 @@
 # load a YT playlist and feed it into the YT downloader software already on my PC
 # move that file to a directory, and rename it to something readable if possible
 
-import re, os, subprocess, time
+import re, os, subprocess, time, shutil
 
-# sets YT downloader dir depending on if running on Home PC or laptop
-def tell_me_which_directory():
+
+def get_ytdl_dir():
     if os.path.exists(r"C:\Program Files\StableBit\DrivePool"):
-        print("Kev's Home PC detected, setting YT Downloader directory accordingly\n")
+        print("Kev's Home PC detected, setting YT Downloader and Plex library directories accordingly\n")
         direc = r"NEED TO ADD THIS"   # note this this the hardcoded directory for when working on Home PC
     else:
         print("This isn't Kev's Home PC, must be laptop, so setting YT Downloader directory accordingly\n")
         direc = r"C:\Users\Admin\Desktop\YT downloader"
+    return direc
+
+
+def get_library_dir():
+    if os.path.exists(r"C:\Program Files\StableBit\DrivePool"):
+        direc = r"NEED TO ADD THIS"   # note this this the hardcoded directory for when working on Home PC
+    else:
+        direc = r"C:\Users\Admin\Desktop\YT downloader\library_dir"
     return direc
 
 
@@ -36,7 +44,7 @@ for id in set_list:
 #print(URLs_list)
 
 
-ytdl_path = tell_me_which_directory()   # define YT Downloader dir depending on if Home PC or laptop
+ytdl_path = get_ytdl_dir()   # define YT Downloader dir depending on if Home PC or laptop
 
 exe = "youtube-dl.exe"
 args = URLs_list[0]             # this uses just the first URL in the list
@@ -45,28 +53,20 @@ to_dl = exe_with_path + " " + args
 #subprocess.call(to_dl)
 #time.sleep(20)
 
-def get_video_filename(fname, folname, abspath):
-    movie_extensions = ['.mov', '.mp4', '.mkv', '.avi', '.flv']
-    basename, ext = os.path.splitext(fname) # isolate basename and extensions
-    if ext in movie_extensions:
-        # print('Movie file detected in {}: {}'.format(folname, fname))
-        fname_with_path = os.path.join(abspath, folname, fname)    # new name incl path
-        return fname_with_path
-
 os.chdir(ytdl_path) # change cwd to the desired directory
 abspath = os.path.abspath('.')  # define abspath
-movie_files_detected = []
-for folderName, subfolders, filenames in os.walk(ytdl_path):
-    for filename in filenames:
-        if get_video_filename(filename, folderName, abspath) != None:
-            movie_files_detected.append(get_video_filename(filename, folderName, abspath))
-print('Video files found: {}'.format(movie_files_detected))
+lib_dir = get_library_dir()     # define library dir (depends which PC I'm on)
+movie_extensions = ['.mov', '.mp4', '.mkv', '.avi', '.flv']
+for filename in os.listdir(ytdl_path):  # for each file in folder
+    basename, ext = os.path.splitext(filename) # isolate basename and extensions
+    if ext in movie_extensions:
+        original_fullname = os.path.join(abspath, filename)
+        new_fullname = os.path.join(lib_dir, filename)
+        # print('Original fullname is {} \n New fullname is {}\n'.format(original_fullname, new_fullname))
+        print('moving {} to {}'.format(original_fullname, new_fullname))
+        shutil.move(original_fullname, new_fullname)    # move the video files to the library folder
+
 
 # TO DO: currently this script only uses the first ID / URL detected. Need to tell it to use only newly found ones
-# TO DO: Move video files to Plex Library directory
 # TO DO: send email to kevinjpickett@gmail.com ; or send PB notification, to notify me
 # TO DO: set to run at a particular time of day
-
-
-
-
