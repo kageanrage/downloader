41328904a04d66ac337db95807dc3637e477c9ad
kageanrage <kevinjpickett@gmail.com>
2017-06-05 12:43:56 +1000

    added download ability

--- downloader.py
+++ downloader.py
@@ -1,7 +1,7 @@
 # load a YT playlist and feed it into the YT downloader software already on my PC
 # move that file to a directory, and rename it to something readable if possible
 
-import re
+import re, os, subprocess
 
 playlist = r'https://www.youtube.com/playlist?list=PLnwDsHHQyShyCo7o0hUbG23my2ms_Qp_B'
 
@@ -19,5 +19,16 @@ for i in range(0, len(mo) - 1):
     ID_list.append(mo[i][1])    # add all video IDs to ID_list
 
 set_list = set(ID_list)     # de-dupe by converting list to set
-print(set_list)
+#print(set_list)
 
+URLs_list = []
+for id in set_list:
+    URLs_list.append("https://www.youtube.com/watch?v=" + id)
+#print(URLs_list)
+
+
+os.chdir(r"C:\Users\Admin\Desktop\YT downloader")
+args = "youtube-dl.exe " + URLs_list[0]
+subprocess.call(args)
+
+# this is now working - I can add sophistication around sourcing directory name etc later
