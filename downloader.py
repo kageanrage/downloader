d964b859834f9b506aedb2fd60d32825dd02a4dc
kageanrage <kevinjpickett@gmail.com>
2017-06-08 12:48:56 +1000

    more refactoring, added detail to email send

--- downloader.py
+++ downloader.py
@@ -59,14 +59,44 @@ def generate_mo(string_to_search):
     return match_object
 
 
-def send_email(u, p, recipient, subject, body):
+def get_URLs_list(m_o):
+    ytid_list = set([m_o[i][1] for i in range(0, len(m_o) - 1)])  # add all found video IDs to ID_list
+    URLs_list = ["https://www.youtube.com/watch?v=" + ytid for ytid in ytid_list]   # expand yt ids to yt URLs
+    print('URLs list is: {}'.format(URLs_list))
+    return URLs_list
+
+
+def download_videos(urlz):
+    exe = "youtube-dl.exe"
+    args = urlz[0]  # this uses just the first URL in the list
+    exe_with_path = os.path.join(ytdl_path, exe)
+    command = exe_with_path + " " + args
+    print('TEST - pretending to download {}'.format(command))
+    # subprocess.call(command)
+    # time.sleep(20)
+
+
+def move_videos(ytdl_path2, movie_extensions2, lib_dir2):
+    os.chdir(ytdl_path2)  # change cwd to the desired directory
+    abspath_yt = os.path.abspath('.')  # define abspath
+    for filename in os.listdir(ytdl_path2):  # for each file in folder
+        basename, ext = os.path.splitext(filename)  # isolate basename and extensions
+        if ext in movie_extensions2:
+            original_fullname = os.path.join(abspath_yt, filename)
+            new_fullname = os.path.join(lib_dir2, filename)
+            # print('Original fullname is {} \n New fullname is {}\n'.format(original_fullname, new_fullname))
+            print('moving {} to {}'.format(original_fullname, new_fullname))
+            shutil.move(original_fullname, new_fullname)  # move the video files to the library folder
+
+
+def send_email(u, p, recipient, subject, urlz):
 
     gm_u = u
     gm_p = p
     FROM = u
     TO = recipient if type(recipient) is list else [recipient]
     SUBJECT = subject
-    TEXT = body
+    TEXT = 'The following videos have been downloaded: {}'.format(urlz)
 
     # Prepare actual message
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
@@ -86,39 +116,20 @@ def send_email(u, p, recipient, subject, body):
 machine = which_pc()    # determine if on Home PC or laptop
 config_file = get_config_file()     # access config file
 u_and_p = get_em_access()   # returns u p tuple for em access
-# send_email(u_and_p[0], u_and_p[1], u_and_p[0], 'subject text is this', 'message body is this text') # send em
+
 playlist = get_playlist_url()  # get yt playlist URL, not yet in use
 localFile = open(r'html\yt.html', 'r')  # test mode - uses pre-downloaded HTML
 local_string = str((localFile.read()))  # test mode - converts downloaded HTML to string
-mo = generate_mo(local_string)
-
-ytid_list = set([mo[i][1] for i in range(0, len(mo) - 1)])  # add all found video IDs to ID_list
-URLs_list = ["https://www.youtube.com/watch?v=" + ytid for ytid in ytid_list]   # expand yt ids to yt URLs
-print('URLs list is: {}'.format(URLs_list))
-
+mo = generate_mo(local_string)  # pass in string to search and return match object
+urls = get_URLs_list(mo)    # pass in mo and return list of YT video URLs
 ytdl_path = get_ytdl_dir()   # define YT Downloader dir depending on if Home PC or laptop
-
-exe = "youtube-dl.exe"
-args = URLs_list[0]             # this uses just the first URL in the list
-exe_with_path = os.path.join(ytdl_path, exe)
-to_dl = exe_with_path + " " + args
-#subprocess.call(to_dl)
-#time.sleep(20)
-
-os.chdir(ytdl_path) # change cwd to the desired directory
+download_videos(urls)   # download the videos, CURRENTLY WILL ONLY DL FIRST IN LIST
 abspath = os.path.abspath('.')  # define abspath
 lib_dir = get_library_dir()     # define library dir (depends which PC I'm on)
 movie_extensions = ['.mov', '.mp4', '.mkv', '.avi', '.flv']
-for filename in os.listdir(ytdl_path):  # for each file in folder
-    basename, ext = os.path.splitext(filename) # isolate basename and extensions
-    if ext in movie_extensions:
-        original_fullname = os.path.join(abspath, filename)
-        new_fullname = os.path.join(lib_dir, filename)
-        # print('Original fullname is {} \n New fullname is {}\n'.format(original_fullname, new_fullname))
-        print('moving {} to {}'.format(original_fullname, new_fullname))
-        shutil.move(original_fullname, new_fullname)    # move the video files to the library folder
+move_videos(ytdl_path, movie_extensions, lib_dir)  # TO DO: currently this script only uses the first ID / URL detected. Need to tell it to use only newly found ones
+send_email(u_and_p[0], u_and_p[1], u_and_p[0], 'subject text is this', urls)  # send em
 
-
-# TO DO: currently this script only uses the first ID / URL detected. Need to tell it to use only newly found ones
-# TO DO: send email to kevinjpickett@gmail.com ; or send PB notification, to notify me
+# TO DO: enable fresh downloading of HTML
+# TO DO: ensure only newly-added IDs / URLs are processed
 # TO DO: set to run at a particular time of day
