ca49138a7d8a5a8772209f57304c5dbae04ee991
kageanrage <kevinjpickett@gmail.com>
2017-06-09 06:15:47 +1000

    first successful test of multiple video download & move

--- downloader.py
+++ downloader.py
@@ -13,11 +13,12 @@ def which_pc():
 
 def get_ytdl_dir():
     if machine == 'desktop':
-        print("Kev's Home PC detected, setting YT Downloader and Plex library directories accordingly\n")
         direc = r"C:\Users\kevin\Desktop\Youtube Downloader"   # note this this the hardcoded directory for when working on Home PC
+        print("Kev's Home PC detected, setting YT Downloader dir to {}".format(direc))
     else:
-        print("This isn't Kev's Home PC, must be laptop, so setting YT Downloader directory accordingly\n")
+
         direc = r"C:\Users\Admin\Desktop\YT downloader"
+        print("This isn't Kev's Home PC, must be laptop, so setting YT Downloader dir to {}\n".format(direc))
     return direc
 
 
@@ -26,6 +27,14 @@ def get_library_dir():
         direc = r"C:\Github local repos\test"   # TEST - note this this the hardcoded directory on Home PC
     else:
         direc = r"C:\Users\Admin\Desktop\YT downloader\library_dir"
+    return direc
+
+
+def get_base_dir():
+    if machine == 'desktop':
+        direc = r"C:\Github local repos\downloader"   # TEST - note this this the hardcoded directory on Home PC
+    else:
+        direc = r"C:\KP Python\downloader"
     return direc
 
 
@@ -67,13 +76,16 @@ def get_URLs_list(m_o):
 
 
 def download_videos(urlz):
+    os.chdir(get_ytdl_dir())
     exe = "youtube-dl.exe"
-    args = urlz[0]  # this uses just the first URL in the list
     exe_with_path = os.path.join(ytdl_path, exe)
-    command = exe_with_path + " " + args
-    print('TEST - pretending to download {}'.format(command))
-    # subprocess.call(command)
-    # time.sleep(20)
+    for url in urlz:
+        args = url
+        command = exe_with_path + " " + args
+        print('TEST - pretending to download {}'.format(command))
+        subprocess.call(command)
+        print('Sleeping for 3 sec...')
+        time.sleep(3)
 
 
 def move_videos(ytdl_path2, movie_extensions2, lib_dir2):
@@ -87,6 +99,24 @@ def move_videos(ytdl_path2, movie_extensions2, lib_dir2):
             # print('Original fullname is {} \n New fullname is {}\n'.format(original_fullname, new_fullname))
             print('moving {} to {}'.format(original_fullname, new_fullname))
             shutil.move(original_fullname, new_fullname)  # move the video files to the library folder
+        else:
+            print('{} not a video file so not moving'.format(filename))
+
+
+def check_for_new_urls(urlz):
+    os.chdir(get_base_dir())
+    urls_file = open(os.path.join(os.getcwd(), 'urls.txt'), 'r')
+    content = urls_file.read()
+    new_urlz = []
+    for url in urlz:
+        if url not in content:
+            urls_file.close()
+            urls_file = open(os.path.join(os.getcwd(), 'urls.txt'), 'a')
+            print('New URL found and added to URLs.txt: {}'.format(url))
+            urls_file.write(str(url) + '\n')
+            new_urlz.append(url)
+    urls_file.close()
+    return new_urlz
 
 
 def send_email(u, p, recipient, subject, urlz):
@@ -123,13 +153,17 @@ local_string = str((localFile.read()))  # test mode - converts downloaded HTML t
 mo = generate_mo(local_string)  # pass in string to search and return match object
 urls = get_URLs_list(mo)    # pass in mo and return list of YT video URLs
 ytdl_path = get_ytdl_dir()   # define YT Downloader dir depending on if Home PC or laptop
-download_videos(urls)   # download the videos, CURRENTLY WILL ONLY DL FIRST IN LIST
-abspath = os.path.abspath('.')  # define abspath
-lib_dir = get_library_dir()     # define library dir (depends which PC I'm on)
-movie_extensions = ['.mov', '.mp4', '.mkv', '.avi', '.flv']
-move_videos(ytdl_path, movie_extensions, lib_dir)  # TO DO: currently this script only uses the first ID / URL detected. Need to tell it to use only newly found ones
-send_email(u_and_p[0], u_and_p[1], u_and_p[0], 'Py Script - new vids DLed from YT', urls)  # send em
+new_urls = check_for_new_urls(urls) # return any URLs which were not in the txt file, as a list, and add them to txt
+download_videos(new_urls)   # download the videos, CURRENTLY WILL ONLY DL FIRST IN LIST
+if len(new_urls) > 0:
+    abspath = os.path.abspath('.')  # define abspath
+    lib_dir = get_library_dir()     # define library dir (depends which PC I'm on)
+    movie_extensions = ['.mov', '.mp4', '.mkv', '.avi', '.flv', '.webm']
+    move_videos(ytdl_path, movie_extensions, lib_dir)  # TO DO: currently this script only uses the first ID / URL detected. Need to tell it to use only newly found ones
+    send_email(u_and_p[0], u_and_p[1], u_and_p[0], 'Py Script - new vids DLed from YT', new_urls)  # send em
+
 
 # TO DO: enable fresh downloading of HTML
-# TO DO: ensure only newly-added IDs / URLs are processed. Add them to xls, sql or txt file to serve as long term memory
+
+
 # TO DO: set to run at a particular time of day
