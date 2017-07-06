551b2b70e9a1de49bb9240ecf3b41a9d3d2f55bd
kageanrage <kevinjpickett@gmail.com>
2017-07-06 16:40:10 +1000

    moved from config text file to config.py and updated gitignore

--- downloader.py
+++ downloader.py
@@ -2,36 +2,14 @@
 # move that file to a directory, and rename it to something readable if possible
 
 import re, os, subprocess, time, shutil, smtplib, requests, bs4, sys
+from config import Config
 
 
 def which_pc():
-    if os.path.exists(r"C:\Program Files\StableBit\DrivePool"):
+    if os.path.exists(cfg.which_pc_path_to_check):
         return 'desktop'
     else:
         return 'laptop'
-
-
-def get_config_file():
-    if machine == 'desktop':
-        config = r"C:\Github local repos\dont_share\config.txt"
-    else:
-        config = r"C:\KP Python\dont_share\config.txt"
-    return config
-
-
-def get_em_access():
-    f = open(config_file, 'r')  # import config info
-    f_str = str((f.read()))  # import config info
-    p = f_str[19:31]
-    u = f_str[44:67]
-    return u, p
-
-
-def get_playlist_url():
-    f = open(config_file, 'r')  # import config info
-    f_str = str((f.read()))  # import config info
-    url = f_str[83:155]
-    return url
 
 
 def get_ytdl_dir():
@@ -151,7 +129,7 @@ def get_local_string():
         return string
 
     else:
-        res = requests.get(playlist)
+        res = requests.get(cfg.playlist)
         data = res.text
         soup = bs4.BeautifulSoup(data, "lxml").encode("ascii") # encode is to neutralise weird characters
         the_string = str(soup)
@@ -165,10 +143,9 @@ def get_local_string():
 
 test_mode = False
 
+cfg = Config()
+
 machine = which_pc()    # determine if on Home PC or laptop
-config_file = get_config_file()     # access config file
-u_and_p = get_em_access()   # returns u p tuple for em access
-playlist = get_playlist_url()  # get yt playlist URL, not yet in use
 local_string = get_local_string()
 mo = generate_mo(local_string)  # pass in string to search and return match object
 urls = get_URLs_list(mo)    # pass in mo and return list of YT video URLs
@@ -180,4 +157,4 @@ if len(new_urls) > 0:
     lib_dir = get_library_dir()     # define library dir (depends which PC I'm on)
     movie_extensions = ['.mov', '.mp4', '.mkv', '.avi', '.flv', '.webm']
     move_videos(ytdl_path, movie_extensions, lib_dir)  # TO DO: currently this script only uses the first ID / URL detected. Need to tell it to use only newly found ones
-    send_email(u_and_p[0], u_and_p[1], u_and_p[0], 'Py Script - new vids DLed from YT', new_urls)  # send em
+    send_email(cfg.my_gmail_uname, cfg.my_gmail_pw, cfg.my_gmail_uname, 'Py Script - new vids DLed from YT', new_urls)  # send em
